ca7627ec00b1df497134c78c2af6d78fd2f01b9a
Eugenia Korolik <iceyasher@gmail.com>
2016-04-30 21:09:29 +0300

    Initial commit

---
+++ Lab4/MainWindow.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Gtk;
+using Lab4;
+
+public partial class MainWindow: Gtk.Window
+{
+	CurrencyList Currencies;
+	enum Valutes { Ruble, Euro, Dollar }
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		var currency = new ReadCurrency ("http://www.cbr.ru/scripts/XML_daily.asp");
+		Currencies = currency.GetCurrencyList ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnConverButtonClicked (object sender, EventArgs e)
+	{
+		string inputStr = FromInputLabel.Text;
+
+		if (inputStr.Length == 0)
+			return;
+
+		if (inputStr.Contains ("."))
+			inputStr = inputStr.Replace (".", ",");
+
+		double input = Double.Parse (inputStr);
+
+		switch (FromCurrency.Active) {
+		case (int)Valutes.Ruble:
+			{
+				switch (ToCurrency.Active) {
+				case (int)Valutes.Euro:
+					ToInputLabel.Text = (input * (1 / Currencies.RubleToEuro)).ToString ();
+					break;
+
+				case (int)Valutes.Dollar:
+					ToInputLabel.Text = (input * (1 / Currencies.RubleToDollar)).ToString ();
+					break;
+				}
+			}
+			break;
+
+		case (int)Valutes.Euro:
+			{
+				switch (ToCurrency.Active) {
+				case (int)Valutes.Ruble:
+					ToInputLabel.Text = (input * Currencies.RubleToEuro).ToString ();
+					break;
+
+				case (int)Valutes.Dollar:
+					ToInputLabel.Text = (input * Currencies.DollarToEuro).ToString ();
+					break;
+				}
+			}
+			break;
+
+		case (int)Valutes.Dollar:
+			{
+				switch (ToCurrency.Active) {
+				case (int)Valutes.Ruble:
+					ToInputLabel.Text = (input * Currencies.RubleToDollar).ToString ();
+					break;
+
+				case (int)Valutes.Euro:
+					ToInputLabel.Text = (input * (1 / Currencies.DollarToEuro)).ToString ();
+					break;
+				}
+			}
+			break;
+		}
+	}
+
+	protected void OnFromCurrencyChanged (object sender, EventArgs e)
+	{
+		if (FromCurrency.Active == ToCurrency.Active)
+		{
+			ToForbidden.Show ();
+			ToArrow.Hide ();
+			ConvertButtonHide ();
+		}
+		else if (ToForbidden.Visible)
+		{
+			ToForbidden.Hide ();
+			ToArrow.Show ();
+			ConvertButtonShow ();
+		}
+	}
+
+	protected void OnToCurrencyChanged (object sender, EventArgs e)
+	{
+		if (FromCurrency.Active == ToCurrency.Active)
+		{
+			ToForbidden.Show ();
+			ToArrow.Hide ();
+			ConvertButtonHide ();
+		}
+		else if (ToForbidden.Visible)
+		{
+			ToForbidden.Hide ();
+			ToArrow.Show ();
+			ConvertButtonShow ();
+		}
+	}
+
+	protected void OnFromInputLabelChanged (object sender, EventArgs e)
+	{
+		double result = 0;
+		string input = FromInputLabel.Text;
+
+		if (input.Contains ("."))
+			input = input.Replace (".", ",");
+
+		if (!Double.TryParse (input, out result))
+		{
+			ConvertButtonHide ();
+			FromInputError.Show ();
+		}
+		else if (!ConvertButton.Visible)
+		{
+			ConvertButtonShow ();
+			FromInputError.Hide ();
+		}
+	}
+
+	protected void ConvertButtonHide ()
+	{
+		ConvertButton.Hide ();
+		ConvertButtonIcon.Hide ();
+	}
+
+	protected void ConvertButtonShow ()
+	{
+		ConvertButton.Show ();
+		ConvertButtonIcon.Show ();
+	}
+
+}
---
+++ Lab4/ReadCurrency.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace Lab4
+{
+	public class ReadCurrency
+	{
+		readonly string Url;
+
+		public ReadCurrency (string url)
+		{
+			Url = url;
+		}
+
+		public CurrencyList GetCurrencyList ()
+		{
+			var currencyXML = GetXMLFromSite ();
+
+			double ruble2euro = 0, ruble2dollar = 0;
+
+			foreach (XElement valute in currencyXML.Root.Elements ())
+			{
+				if (valute.Element ("CharCode").Value == "USD")
+					ruble2dollar = Double.Parse (valute.Element ("Value").Value);
+
+				if (valute.Element ("CharCode").Value == "EUR")
+					ruble2euro = Double.Parse (valute.Element ("Value").Value);
+			}
+
+			return new CurrencyList (ruble2euro, ruble2dollar);
+		}
+
+		public XDocument GetXMLFromSite ()
+		{
+			XDocument result;
+
+			try
+			{
+				var request = WebRequest.Create (Url);
+
+				var stream = request.GetResponse().GetResponseStream();
+
+				result = XDocument.Load (new StreamReader(stream, Encoding.GetEncoding (1251)));
+
+				stream.Close ();
+				request.Abort ();
+
+				result.Save("currencyhash.xml");
+			}
+			catch (WebException)
+			{
+				result = XDocument.Parse (File.ReadAllText ("currencyhash.xml", Encoding.GetEncoding (1251)));
+			}
+
+			return result;
+		}
+	}
+
+	public struct CurrencyList
+	{
+		public readonly double RubleToEuro;
+		public readonly double RubleToDollar;
+		public readonly double DollarToEuro;
+
+		public CurrencyList (double ruble2euro, double ruble2dollar)
+		{
+			RubleToEuro = ruble2euro;
+			RubleToDollar = ruble2dollar;
+			DollarToEuro = RubleToEuro / RubleToDollar;
+		}
+	}
+}
